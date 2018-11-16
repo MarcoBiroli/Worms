5c60d0f9223edd5aa6984078e9fc0a705337d4d3
MarcoBiroli <marco.biroli@polytechnique.edu>
2018-11-16 14:16:59 +0100

    parameters

--- Physics/RigidBody.h
+++ Physics/RigidBody.h
@@ -1,9 +1,10 @@
 #pragma once
+#include "Force.h"
 
 class RigidBody{
     private:
         double mass = 0, x = 0, y = 0, vx = 0, vy = 0, ax = 0, ay = 0;
+        Force currentForce;
     public:
-
-
+        void addForce(Force F);
 };
