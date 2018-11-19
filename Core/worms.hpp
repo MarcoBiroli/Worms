7e7cd9f4dbeabe7b85e1bdabac0bc4aa68dbe0ca
TGazel <44865341+TGazel@users.noreply.github.com>
2018-11-19 16:05:34 +0100

    Few modifications (move, jump)

--- Core/worms.hpp
+++ Core/worms.hpp
@@ -10,11 +10,13 @@ class Worms: public RigidBody {
 
       bool isAlive();
 
+      void move();//move right (if right=True, left if right=false) by modyfying x-velocity  ***(how deal with slopes, etc?)
+      void jump();//adds upwards  ***(velocity or acceleration?)
       void pickUpWeapon(int weapon_ID, int ammo);
 
       void weaponSelect(int weapon_ID);
       void changeAngle(double angle_change); //modifes weapon angle attribute of worms by angle_change (clockwise is positive, counter-clockwise is negative)
-      void fireWeapon(double power, std::vector* projectile_list); //
+      void fireWeapon(double power, std::vector* projectile_list); //with the power from the user input and the angle stored in the Worms class, create projectile(s) specific to current_weapon and add them to the list of projectiles of the game loop
 
     private:
       int health;
