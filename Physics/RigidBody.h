44a5cb3250af0984929221f8ca9f6b3c131e187b
MarcoBiroli <marco.biroli@polytechnique.edu>
2018-11-22 11:21:49 +0100

    Update of Rigidbody.h

--- Physics/RigidBody.h
+++ Physics/RigidBody.h
@@ -4,7 +4,17 @@
 class RigidBody{
     private:
         double mass = 0, x = 0, y = 0, vx = 0, vy = 0, ax = 0, ay = 0;
-        Force currentForce;
+        double bouciness_f = 0;
+        QVector currentForce = QVector(0, 0);
+        bool stable = false; 
     public:
         void addForce(Force F);
+        double distance(RigidBody other);
+        void simulate(double dt);
+
+
+        void setbounciness();
+        void setm(); setx(); sety(); setvx(); setvy(); 
+
+
 };
