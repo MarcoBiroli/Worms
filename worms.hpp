f5e2099a345d3c4bda251e598ff79c60f57cc6e9
TGazel <44865341+TGazel@users.noreply.github.com>
2018-11-22 11:11:05 +0100

    Change team storing

---
+++ worms.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+#include "RigidBody.hpp"
+
+class Worms: public RigidBody {
+    public:
+      Worms(int team_number, std::string personal_name, int health, double mass, double x, double y, ...): RigidBody(mass, x, y, ...);
+
+      bool isAlive();
+
+      void move(bool right);//move right (if right=True, left if right=false) by modyfying x-velocity  ***(how deal with slopes, etc?)
+      void jump();//adds upwards velocity
+      void pickUpWeapon(int weapon_ID, int ammo);
+
+      void weaponSelect(int weapon_ID);
+      void changeAngle(double angle_change); //modifes weapon angle attribute of worms by angle_change (clockwise is positive, counter-clockwise is negative)
+      void fireWeapon(double power, std::vector* projectile_list); //with the power from the user input and the angle stored in the Worms class, create projectile(s) specific to current_weapon and add them to the list of projectiles of the game loop
+
+    private:
+      int health;
+      int current_weapon;
+      int weapon_angle;
+
+      int* weapons[];
+      
+      const int team_number; //team number
+      const std::string personal_name;
+
+
+};
