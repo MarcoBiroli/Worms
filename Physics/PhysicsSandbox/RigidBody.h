b05fbca574b893a5f1b69cbfc990365ba703a69b
Alessandro Pacco <45063975+AlPa0@users.noreply.github.com>
2018-11-24 16:58:46 +0100

    Update RigidBody.h

--- Physics/PhysicsSandbox/RigidBody.h
+++ Physics/PhysicsSandbox/RigidBody.h
@@ -4,16 +4,15 @@
 #include <QDebug>
 
 
-//me and camillo need to add comments here
 
 class RigidBody:public Collider{
 private:
+    double mass = 0, vx = 0, vy = 0, ax = 0, ay = 0;
     QPair<double, double> currentForce = QPair<double, double>(0, 0);
+    double bounciness_f = 0;
     bool stable = false;
     
 public:
-    double bounciness_f = 0;
-    double mass = 0, vx = 0, vy = 0, ax = 0, ay = 0;
     RigidBody(double imass, double ix, double iy, double vx0, double vy0, double ax0, double ay0, int iid, QImage map);
     RigidBody(double imass, double ix, double iy);
     
@@ -23,14 +22,20 @@ public:
     double distance(RigidBody other);
     void simulate(double dt);
     
-    /*void setbounciness();
-    void setm();
-    void setx();
-    void sety();
-    void setvx();
-    void setvy();
-    void setax();
-    void setay();
-    bool getstable();*/
+    void setbounciness(double b);
+    void setm(double m);
+    void setvx(double v_x);
+    void setvy(double v_y);
+    void setax(double a_x);
+    void setay(double a_y);
+    void setstable(bool a);
+
+    bool getstable();
+    double getbounciness();
+    double getm();
+    double getvx();
+    double getvy();
+    double getax();
+    double getay();
     
 };
