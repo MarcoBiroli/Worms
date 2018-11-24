1ec640931850f5772d2585daf93a2839302e8394
MarcoBiroli <32924635+MarcoBiroli@users.noreply.github.com>
2018-11-24 16:57:09 +0100

    Update main.cpp

--- Physics/PhysicsSandbox/main.cpp
+++ Physics/PhysicsSandbox/main.cpp
@@ -3,6 +3,7 @@
 #include "ground.h"
 #include "Collider.h"
 #include "RigidBody.h"
+#include "PhysicsEngine.h"
 #include <QTime>
 
 int main(int argc, char *argv[])
@@ -13,55 +14,46 @@ int main(int argc, char *argv[])
         QGraphicsScene *scene = new QGraphicsScene();
         //Put a graphics view inside of the window
         QGraphicsView *view = new QGraphicsView(scene);
+
+        PhysicsEngine Engine(QPair<double, double>(3, 9.81));
+
         //Create a Ground of size (500x500)
-        Ground *ground = new Ground(500, 500);
+        Ground ground = Ground(1440, 900);
+        Engine.add_Collider(&ground);
         //Create a Rigidbody
-        RigidBody* body = new RigidBody(1, 250, 0);
-        body->bounciness_f = 0.5;
-
-
+        RigidBody* body = Engine.create_rigidbody(1, 250, 0);
+        body->bounciness_f = 0.8;
 
         //Delete everything in the ground in radius of 100px aroud (250,250)
-        ground->circ_delete(250, 250, 100);
+        ground.circ_delete(250, 250, 100);
 
         //Add the ground image to the window so it gets displayed
-        QGraphicsPixmapItem *tmp_ground = new QGraphicsPixmapItem(QPixmap::fromImage(ground->colliding_map));
+        QGraphicsPixmapItem *tmp_ground = new QGraphicsPixmapItem(QPixmap::fromImage(ground.colliding_map));
         scene-> addItem(tmp_ground);
 
-        QGraphicsPixmapItem *tmp = new QGraphicsPixmapItem(QPixmap::fromImage(body->colliding_map)); //?
+        QGraphicsPixmapItem *tmp = new QGraphicsPixmapItem(QPixmap::fromImage(body->colliding_map));
 
         tmp->setFlag(QGraphicsItem::ItemIsMovable);
         scene->addItem(tmp);
         tmp->setPos(body->getX(), body->getY());
 
-
-
-
         QTime lastUpdate= QTime::currentTime();
         int timeSinceLastUpdate = QTime::currentTime().msecsTo(lastUpdate);
         double update_time = 10;
-
 
         while(true){
             a.processEvents();
 
             timeSinceLastUpdate = lastUpdate.msecsTo(QTime::currentTime());
             if(timeSinceLastUpdate>update_time){
-                body->addForce(QPair<double, double>(0, 9.81));
-
-                for(int i = 0; i< 10; i++)
-                {
-                    body->simulate(timeSinceLastUpdate);
-
-                    if (body->check_collision(*ground).first == true){
-                        body->bounce(body->check_collision(*ground).second);
-                        }
+                for(int i = 0; i < 5; i++){
+                    Engine.update(timeSinceLastUpdate);
                 }
                 tmp->setPos(body->getX(), body->getY());
                 lastUpdate = QTime::currentTime();
             }
-
             view->show();
         }
         return a.exec();
 }
+
