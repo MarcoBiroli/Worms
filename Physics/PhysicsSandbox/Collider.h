c8d4912634459c03165a46bb69ecd370af1667a5
MegiDervishi <45063968+MegiDervishi@users.noreply.github.com>
2018-11-24 16:50:18 +0100

    Update Collider.h

--- Physics/PhysicsSandbox/Collider.h
+++ Physics/PhysicsSandbox/Collider.h
@@ -3,43 +3,40 @@
 #include <QPixmap>
 #include <QGraphicsPixmapItem>
 
+/*The collider class which takes care of the collision of two or more rigid bodies*/
+
 class Collider{
     private:
-        int id;
-        double cmx, cmy;
+        int id; //Collider ID
+        double x = 0, y = 0; //default position of x and y
+
+    protected:
+        double cmx, cmy; //Central of mass x and y
+        QImage colliding_map = QImage(32,32,QImage::Format_RGB32); //default colliding map
+
     public:
-        double x = 0, y = 0; //change to private and write their get functions
-        QImage colliding_map = QImage(32,32,QImage::Format_RGB32); //change to black
+        //Constructors
+
         Collider();
         Collider(double ix, double iy, QImage map, int iid);
         Collider(double ix, double iy, int iid);
+
+        //get and set private variables
+
+        double getId();
+        void setId(int iid);
+
+        double getX();
+        double getY();
+
+        void setX(double x1);
+        void setY(double y1);
+
+        QImage get_map();
         void set_map(QImage map);
 
+        //check_collision method returns (bool is_colliding, Normal_Force); Normal_Force = (norm_x, norm_y)
 
-        QPair<bool, QPair<double, double>> check_collision(Collider &other) //return type iscolliding , normal vector
-        {
-            bool colliding = false;
-            double cm_otherx = 0, cm_othery = 0;
-            int nb_colliding_pixels = 0;
-            for (int i = 0; i < this->colliding_map.width(); i++) {
-                for (int j = 0; j < this->colliding_map.height(); j++){
-                    if (this->colliding_map.pixelColor(i,j) == Qt::black){
-                        if (other.x <= x+i <= other.x + other.colliding_map.width() && other.y <= y + j <= other.y + other.colliding_map.height()){
-                            if (other.colliding_map.pixelColor(x+i-other.x, y+j - other.y) == Qt::black){
-                                colliding = true;
-                                cm_otherx ++; cm_othery++; nb_colliding_pixels++;
-                            }
-                        }
-                    }
-                }
-            }
-            cm_otherx = cm_otherx / nb_colliding_pixels;
-            cm_othery = cm_othery / nb_colliding_pixels;
-            return QPair<bool, QPair<double, double>> (colliding, QPair<double, double> (cmx-cm_otherx,cmy-cm_othery));
-        }
-        double getX(){return this->x;}
-        double getY(){return this->y;}
+        QPair<bool, QPair<double, double>> check_collision(Collider &other);
 
-        double getId(){return this->id;}
-        void setId(int iid){this->id = iid;}
 };
