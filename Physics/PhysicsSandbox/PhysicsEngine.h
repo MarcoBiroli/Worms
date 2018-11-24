ee9a0ef35d58beecd4a56182510890c17d752b11
MarcoBiroli <32924635+MarcoBiroli@users.noreply.github.com>
2018-11-24 16:56:27 +0100

    Update PhysicsEngine.h

--- Physics/PhysicsSandbox/PhysicsEngine.h
+++ Physics/PhysicsSandbox/PhysicsEngine.h
@@ -9,12 +9,15 @@ using namespace std;
 
 class PhysicsEngine{
     private:
-        QMap<int, RigidBody> rigidbodies = QMap<int, RigidBody>();
-        QMap<int, Collider> colliders = QMap<int, Collider>();
-        //Ground ground;
+        QMap<int, RigidBody*> rigidbodies = QMap<int, RigidBody*>();
+        QMap<int, Collider*> colliders = QMap<int, Collider*>();
+        QPair<double, double> general_force = QPair<double, double>(0, 0);
     public:
-        void add_RigidBody(RigidBody other);
-        void add_Collider(Collider other);
+        PhysicsEngine();
+        PhysicsEngine(QPair<double, double> gf);
+        void add_RigidBody(RigidBody* other);
+        void add_Collider(Collider* other);
         void update(double dt);
-    
+        RigidBody* create_rigidbody(double imass, double ix, double iy, double vx0, double vy0, double ax0, double ay0, int iid, QImage map);
+        RigidBody* create_rigidbody(double imass, double ix, double iy);
 };
