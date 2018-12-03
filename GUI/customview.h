240918e2a75d94fa213598b2aa0aa46b8d4d7b0d
MarcoBiroli <marco.biroli@polytechnique.edu>
2018-12-03 14:13:08 +0100

    moved curstomview files

---
+++ GUI/customview.h
@@ -0,0 +1,49 @@
+
+#define CUSTOMVIEW_H
+
+#include <QGraphicsView>
+#include <QtWidgets>
+#include <QWidget>
+#include <QObject>
+
+
+class CustomView : public QGraphicsView
+{
+public:
+    CustomView(QWidget* parent);
+protected:
+    virtual void wheelEvent(QWheelEvent *event);
+};
+
+/*
+class SceneView : public QGraphicsView
+{
+public:
+    SceneView(QGraphicsScene* parent);
+protected:
+    virtual void wheelEvent(QWheelEvent *event);
+};
+
+---------------------------------
+
+class Graphics_view_zoom : public QObject {
+  Q_OBJECT
+public:
+  Graphics_view_zoom(QGraphicsView* view);
+  void gentle_zoom(double factor);
+  void set_modifiers(Qt::KeyboardModifiers modifiers);
+  void set_zoom_factor_base(double value);
+
+private:
+  QGraphicsView* _view;
+  Qt::KeyboardModifiers _modifiers;
+  double _zoom_factor_base;
+  QPointF target_scene_pos, target_viewport_pos;
+  bool eventFilter(QObject* object, QEvent* event);
+
+signals:
+  void zoomed();
+};
+
+#endif // CUSTOMVIEW_H
+*/
