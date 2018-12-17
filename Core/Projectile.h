286e60d87d5bdaacd2e9af0d0861e42a2df0af33
Jean-Tiago Hamm <hamm.jeantiago@gmail.com>
2018-12-17 09:53:09 +0100

    code display changes

--- Core/Projectile.h
+++ Core/Projectile.h
@@ -19,24 +19,28 @@ class Projectile : public RigidBody {
         Projectile(std::string name, int weapon_id, double ipower, double bounciness, bool explosion_by_delay, double delay, double explosion_r, double damage, double mass, double x, double y, QPixmap isprite);
 
         Projectile(const Projectile &other);
+
         Projectile* clone();
         //Allows cloning of a projectile.
 
         virtual bool on_collision_do(Collider &other);
-        void explode(Ground &ground, PhysicsEngine &engine, QVector<Projectile*> &projectiles, QVector<Worm*> &worms, QVector<Barrel*> &barrels);
 
-        int get_weapon_id() const;
+        void explode(Ground &ground, PhysicsEngine &engine, QVector<Projectile*> &projectiles, QVector<Worm*> &worms, QVector<Barrel*> &barrels);
         // if explosion condition is met (collosion or delay timeout), call this function.
-        // the function does the following: 
+        // the function does the following:
         // generates damage in explosion_radius, with linear decrease of damage from
         // position of explosion to distance explosion_radius of the center of explosion.
         // destroys all terrain in radius explosion_radius of center of explosion
         // trigger explosion of barrels in the explosion radius
         // destroy the projectile
 
+        int get_weapon_id() const;
+
         void set_inital_position(double x, double y);
+        // set initial position
 
         bool change_delay(double dt);
+        // changes delay
 
 
 
