0fb05ed3a114edd5a4aeeea041eb62c4350180ed
GuillaumeLaine <guillaume.william1@hotmail.com>
2019-01-07 13:38:15 +0100

    merging

--- Core/Projectile.cpp
+++ Core/Projectile.cpp
@@ -74,6 +74,20 @@ void Projectile::explode(Ground &ground, PhysicsEngine &engine, QVector<Projecti
             QPair<double, double> explosion_force = QPair<double, double> (Fx, Fy);
             worm->addForce(explosion_force);
         }
+    for (int i=0; i<projectiles.size(); i++) {
+        Projectile* projectile = projectiles[i];
+        double dist = this->distance(*worm);
+        if (dist <= explosion_radius) {
+            int dmg_dealt = damage - (damage/explosion_radius)*dist;
+            //run explosion animation
+            QPair<double, double> vect_dist =  QPair<double, double> (projectile->getX() - this->x, projectile->getY() - this->y);
+            double Fx = this->repulsion_power*(vect_dist.first/dist)*dmg_dealt/update_time;
+            double Fy = this->repulsion_power*(vect_dist.second/dist)*dmg_dealt/update_time;
+            //Force applied depends on the damage dealt and the distance to the explosion
+            QPair<double, double> explosion_force = QPair<double, double> (Fx, Fy);
+            projectile->addForce(explosion_force);
+        }
+    }
     }
 
     for (int j=0; j<barrels.size(); j++) {
